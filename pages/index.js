60798ba9f8a453f0b82433230a98f47e168e7107
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-02-18 18:03:01 +0100

    add static props to sort post data on build time

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
 
-const Home = () => {
+export const getStaticProps = async () => {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+};
+
+const Home = ({ allPostsData }) => {
   return (
     <Layout home>
       <Head>
@@ -13,6 +23,20 @@ const Home = () => {
         <p>
           <a href="https://nextjs.org/learn">Next.js docs</a>
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
