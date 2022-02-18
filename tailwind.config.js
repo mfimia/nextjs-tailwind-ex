05833f0d59e7ede07ba0a438da7fa52ccecf2d93
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-02-18 13:23:39 +0100

    add purge option

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// remove unused css by specifying the purge option
+module.exports = {
+  purge: [
+    // Use *.tsx if using TypeScript
+    "./pages/**/*.js",
+    "./components/**/*.js",
+  ],
+  // ...
+};
