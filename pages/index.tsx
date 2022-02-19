0ae7cc1773b2c3f3779c61c9bc915293d0694d94
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-02-19 16:55:13 +0100

    add tailwind style component

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,6 +37,13 @@ const Home = ({
             </li>
           ))}
         </ul>
+        <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+          <div className="shrink-0"></div>
+          <div>
+            <div className="text-xl font-medium text-black">Example</div>
+            <p className="text-slate-500">TailwindCSS styles</p>
+          </div>
+        </div>
       </section>
     </Layout>
   );
